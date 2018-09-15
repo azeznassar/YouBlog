0c32afad558aec1c3fdd3db4a00095f5c838e43c
Azez Nassar <azez.nassar@yahoo.com>
2018-09-16 03:57:49 +1200

    added signout and account routes

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, flash, redirect, url_for
-from flask_login import login_user, current_user
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
 from src import app, db, bcrypt
 from src.forms import SignUpForm, LoginForm
 from src.models import User, Post
@@ -59,8 +59,19 @@ def login():
 
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login failed. Verify that you have entered your credentials correctly.', 'danger')
 
     return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Your Account')
